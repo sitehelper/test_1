248b0d1537240cee215646009f1979039d056eaf
Ilya Alkhimenko <i.a.alkhimenko@gmail.com>
2023-04-03 23:56:24 +0700

    About page and adding of gems

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
+#encoding: utf-8
+
+require 'rubygems'
 require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
 
 get '/' do
   @greet = 'Hello, World!'
+  
   erb :index
+end
+
+get '/about' do
+  @message = 'Just some text...'
+
+  erb :about
 end
